5fa8dcf1c9943487522ea3e2abc590d0a363f0bb
zhouzhezz <zorichzorich@gmail.com>
2022-09-05 11:56:15 -0400

    remove utils module

--- utils/pp.ml
+++
@@ -1,6 +0,0 @@
-open Sugar
-
-let mk_colored color str = spf " @{<%s>%s@}" color str
-let mk_red = mk_colored "red"
-let mk_blue = mk_colored "blue"
-let printf = Ocolor_format.printf
--- utils/zlog.ml
+++
@@ -1,6 +0,0 @@
-module Log = Dolog.Log
-
-let init () =
-  Log.set_log_level Log.DEBUG;
-  Log.set_output stdout;
-  ()
