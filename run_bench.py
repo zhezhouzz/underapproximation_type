4c32681bcdbdd2b7b6fdd1824574b1ef4a4f96be
zhouzhezz <zorichzorich@gmail.com>
2022-10-07 16:22:19 -0400

    add script

---
+++ run_bench.py
@@ -0,0 +1,36 @@
+import sys
+import argparse
+import os
+import subprocess
+verbose = True
+
+cmd_prefix = ["dune", "exec", "--", "bin/main.exe"]
+
+workdir = "data/benchmark/"
+
+def invoc_cmd(cmd, output_file):
+    if output_file is not None:
+        # print("{}:{}".format(output_file, type(output_file)))
+        if (verbose):
+            print(" ".join(cmd + [">>", output_file]))
+        with open(output_file, "a+") as ofile:
+            try:
+                subprocess.run(cmd, stdout=ofile)
+            except subprocess.CalledProcessError as e:
+                print(e.output)
+    else:
+        if (verbose):
+            print(" ".join(cmd))
+        try:
+            subprocess.run(cmd)
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+
+if __name__ == '__main__':
+    name = sys.argv[1]
+    dir_str = "{}/{}".format(workdir, name)
+    cmd = cmd_prefix + ["test", "under-type-check",
+                        "{}/{}".format(dir_str, "config.json"),
+                        "{}/{}".format(dir_str, "prog.ml"),
+                        "{}/{}".format(dir_str, "_under.ml")]
+    invoc_cmd(cmd, None)
