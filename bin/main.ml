7e3759409d676eba9426c0568059d767b4fff84f
zhouzhezz <zorichzorich@gmail.com>
2023-02-26 21:09:02 -0500

    remove redundent arg

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 open Core
 open Commands
 
-let command = Command.group ~summary:"main " [ ("test", Ctest.test) ]
+(* let command = Command.group ~summary:"main " [ ("test", Ctest.test) ] *)
 
-let () = Command_unix.run command
+let () = Command_unix.run Ctest.test
