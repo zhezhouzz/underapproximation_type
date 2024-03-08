a7a10588f0419f6405b573110849833e361eb611
zhouzhezz <zorichzorich@gmail.com>
2024-03-08 15:25:56 -0500

    clear the repo

--- ast/refinement.mli
+++
@@ -1,9 +0,0 @@
-module type T = sig
-  include Type.T
-
-  val fv : t -> string list
-  val var_space : t -> string list
-  val subst_id : t -> string -> string -> t
-  val conjunct : t -> t -> t
-  val conjunct_list : t list -> t
-end
--- typecheck/under/litcheck.ml
+++
@@ -1,9 +0,0 @@
-open Languages
-
-(* open Abstraction *)
-(* open Sugar *)
-
-let lit_to_prop_lit x =
-  let open UL in
-  let open Autov.Prop in
-  match x with ConstB b -> ACbool b | ConstI i -> ACint i
