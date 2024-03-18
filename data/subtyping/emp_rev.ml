8c878ff71e8adaa839a13de5ff9ae8e4033be2f6
zhouzhezz <zorichzorich@gmail.com>
2024-03-18 00:33:21 -0400

    add subtyping check

---
+++ data/subtyping/emp_rev.ml
@@ -0,0 +1,7 @@
+let[@assert] rty1 =
+  let s = (0 <= v : [%v: int]) [@over] in
+  (emp v : [%v: int list]) [@under]
+
+let[@assert] rty2 =
+  let s = (0 <= v : [%v: int]) [@over] in
+  (fun ((n [@exists]) : int) -> len v n && n <= s : [%v: int list]) [@under]
