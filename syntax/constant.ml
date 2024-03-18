0c7a39ddc0cb09f889e63b73ca75eea7d4dd3ed8
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2024-03-17 23:54:21 -0400

    Roughly translate a constant into it's type... not sure about constructor support

--- syntax/constant.ml
+++ syntax/constant.ml
@@ -9,6 +9,15 @@ type constant =
   | Dt of string * constant list
 [@@deriving sexp]
 
+let rec constant_to_nt c =
+  let open Nt in
+  match c with
+  | U -> unit_ty
+  | B _ -> bool_ty
+  | I _ -> int_ty
+  | Tu l -> Nt.Ty_tuple (List.map constant_to_nt l)
+  | Dt _ -> failwith "Not implemented"
+
 let compare_constant e1 e2 =
   Sexplib.Sexp.compare (sexp_of_constant e1) (sexp_of_constant e2)
 
