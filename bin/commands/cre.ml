dc9a41f8ccfac9964677be4c5897871887c831ec
zhouzhezz <zorichzorich@gmail.com>
2024-03-25 17:56:47 -0400

    update meta json

--- bin/commands/cre.ml
+++ bin/commands/cre.ml
@@ -11,7 +11,7 @@ let parse = Ocaml5_parser.Frontend.parse
 let preproress meta_config_file source_file () =
   let prim_path = Env.get_prim_path () in
   let s1 = parse ~sourcefile:prim_path.type_decls in
-  let s2 = parse ~sourcefile:prim_path.normalp in
+  let s2 = parse ~sourcefile:prim_path.normal_typing in
   let init_normal_ctx =
     struct_mk_ctx Typectx.emp (ocaml_structure_to_items (s1 @ s2))
   in
@@ -35,36 +35,70 @@ let type_check_ meta_config_file source_file () =
   let () = Env.load_meta meta_config_file in
   let code = preproress meta_config_file source_file () in
   let prim_path = Env.get_prim_path () in
-  let predefine = preproress meta_config_file prim_path.under_basicp () in
+  let predefine = preproress meta_config_file prim_path.coverage_typing () in
   let builtin_ctx = Typing.Itemcheck.gather_uctx predefine in
-  let lemmas = preproress meta_config_file prim_path.lemmas () in
-  let lemmas =
-    List.map (fun x -> x.ty) @@ Typing.Itemcheck.gather_axioms lemmas
+  let axioms = preproress meta_config_file prim_path.axioms () in
+  let axioms =
+    List.map (fun x -> x.ty) @@ Typing.Itemcheck.gather_axioms axioms
   in
-  (* let templates = preproress meta_config_file prim_path.templates () in *)
-  (* let templates = *)
-  (*   List.map (fun x -> x.ty) @@ Typing.Itemcheck.gather_axioms templates *)
-  (* in *)
-  (* let () = Inference.Feature.init_template templates in *)
-  let _ = Typing.Itemcheck.struc_check (lemmas, builtin_ctx) code in
+  let _, rty1 = get_rty_by_name code "rty1" in
+  let _, rty2 = get_rty_by_name code "rty2" in
+  let ctx = FrontendTyped.{ builtin_ctx; local_ctx = emp; axioms } in
+  let res = Subtyping.Subrty.sub_rty_bool ctx (rty1, rty2) in
+  let () = FrontendTyped.pprint_typectx_subtyping emp (rty1, rty2) in
+  let () = Pp.printf "Result: %b\n" res in
+  ()
+
+let rec_arg = "rec_arg"
+
+let handle_template templates =
+  let rec_arg, templates =
+    List.partition (fun x -> String.equal x.x rec_arg)
+    @@ Typing.Itemcheck.gather_axioms templates
+  in
+  let rec_arg =
+    match rec_arg with
+    | [ x ] -> x.ty
+    | _ -> failwith "cannot find builtin rec arg constraints"
+  in
+  let () = Typing.Termcheck.init_rec_arg rec_arg in
+  let templates = List.map (fun x -> x.ty) templates in
+  templates
+
+let handle_lemma axioms =
+  let axioms =
+    List.map (fun x -> x.ty) @@ Typing.Itemcheck.gather_axioms axioms
+  in
+  axioms
+
+let type_check_ meta_config_file source_file () =
+  let () = Env.load_meta meta_config_file in
+  let code = preproress meta_config_file source_file () in
+  let prim_path = Env.get_prim_path () in
+  let predefine = preproress meta_config_file prim_path.coverage_typing () in
+  let builtin_ctx = Typing.Itemcheck.gather_uctx predefine in
+  let axioms = preproress meta_config_file prim_path.axioms () in
+  let axioms = handle_lemma axioms in
+  let templates = preproress meta_config_file prim_path.templates () in
+  let templates = handle_template templates in
+  let () = Inference.Feature.init_template templates in
+  let _ = Typing.Itemcheck.struc_check (axioms, builtin_ctx) code in
   ()
 
 let type_infer_ meta_config_file source_file () =
   let () = Env.load_meta meta_config_file in
   let code = preproress meta_config_file source_file () in
   let prim_path = Env.get_prim_path () in
-  let predefine = preproress meta_config_file prim_path.under_basicp () in
+  let predefine = preproress meta_config_file prim_path.coverage_typing () in
   let builtin_ctx = Typing.Itemcheck.gather_uctx predefine in
-  let lemmas = preproress meta_config_file prim_path.lemmas () in
-  let lemmas =
-    List.map (fun x -> x.ty) @@ Typing.Itemcheck.gather_axioms lemmas
-  in
+  let axioms = preproress meta_config_file prim_path.axioms () in
+  let axioms = handle_lemma axioms in
   let templates = preproress meta_config_file prim_path.templates () in
   let templates =
     List.map (fun x -> x.ty) @@ Typing.Itemcheck.gather_axioms templates
   in
   let () = Inference.Feature.init_template templates in
-  let _ = Typing.Itemcheck.struc_infer (lemmas, builtin_ctx) code in
+  let _ = Typing.Itemcheck.struc_infer (axioms, builtin_ctx) code in
   ()
 
 let print_erase_code meta_config_file source_file () =
--- env/env.ml
+++ env/env.ml
@@ -1,27 +1,12 @@
 open Sexplib.Std
 
-type mode =
-  | Debug of {
-      show_preprocess : bool;
-      show_typing : bool;
-      show_queries : bool;
-      show_solving : bool;
-      show_stat : bool;
-      show_info : bool;
-      show_debug : bool;
-    }
-  | Release
-[@@deriving sexp]
+type mode = DebugTags of string list | Release [@@deriving sexp]
 
 type prim_path = {
-  normalp : string;
-  under_basicp : string;
-  under_randomp : string;
-  underp_dir : string;
-  rev_underp_dir : string;
+  normal_typing : string;
+  coverage_typing : string;
   type_decls : string;
-  lemmas : string;
-  functional_lemmas : string;
+  axioms : string;
   templates : string;
 }
 [@@deriving sexp]
@@ -31,6 +16,9 @@ type meta_config = {
   max_printing_size : int;
   logfile : string;
   resfile : string;
+  abdfile : string;
+  num_quantifier : int;
+  abd_templates : string list;
   prim_path : prim_path;
 }
 [@@deriving sexp]
@@ -41,71 +29,34 @@ type config = { all_mps : string list; underp : string; measure : string }
 let meta_config : meta_config option ref = ref None
 let config : config option ref = ref None
 
-let get_mode () =
-  match !meta_config with
-  | None -> failwith "uninit"
-  | Some config -> config.mode
+let get_meta () =
+  match !meta_config with None -> failwith "uninit" | Some config -> config
 
-let get_max_printing_size () =
-  match !meta_config with
-  | None -> failwith "uninit"
-  | Some config -> config.max_printing_size
+let get_mode () = (get_meta ()).mode
+let get_max_printing_size () = (get_meta ()).max_printing_size
 
-let show_debug_preprocess (f : unit -> unit) =
+let show_log kw (f : unit -> unit) =
   match get_mode () with
-  | Debug { show_preprocess; _ } when show_preprocess -> f ()
+  | DebugTags l when List.exists (String.equal kw) l -> f ()
   | _ -> ()
 
-let show_debug_typing (f : unit -> unit) =
-  match get_mode () with
-  | Debug { show_typing; _ } when show_typing -> f ()
-  | _ -> ()
-
-let show_debug_queries (f : unit -> unit) =
-  match get_mode () with
-  | Debug { show_queries; _ } when show_queries -> f ()
-  | _ -> ()
-
-let show_debug_solving (f : unit -> unit) =
-  match get_mode () with
-  | Debug { show_solving; _ } when show_solving -> f ()
-  | _ -> ()
-
-let show_debug_stat (f : unit -> unit) =
-  match get_mode () with
-  | Debug { show_stat; _ } when show_stat -> f ()
-  | _ -> ()
-
-let show_debug_info (f : unit -> unit) =
-  match get_mode () with
-  | Debug { show_info; _ } when show_info -> f ()
-  | _ -> ()
-
-let show_debug_debug (f : unit -> unit) =
-  match get_mode () with
-  | Debug { show_debug; _ } when show_debug -> f ()
-  | _ -> ()
-
-let get_resfile () =
-  match !meta_config with
-  | None -> failwith "get_resfile"
-  | Some config -> config.resfile
-
-let get_prim_path () =
-  match !meta_config with
-  | None -> failwith "uninited prim path"
-  | Some config -> config.prim_path
+let show_debug_preprocess = show_log "preprocess"
+let show_debug_result = show_log "result"
+let show_debug_typing = show_log "typing"
+let show_debug_queries = show_log "queries"
+let show_debug_minterms = show_log "minterms"
+let show_debug_solving = show_log "solving"
+let show_debug_stat = show_log "stat"
+let show_debug_info = show_log "info"
+let show_debug_debug = show_log "debug"
+let get_resfile () = (get_meta ()).resfile
+let get_prim_path () = (get_meta ()).prim_path
+let get_uninterops () = (get_meta ()).abd_templates
 
 let get_measure () =
   match !config with
   | None -> failwith "uninited prim path"
   | Some config -> config.measure
-
-let get_randomp_path () = (get_prim_path ()).under_randomp
-let known_mp : string list option ref = ref None
-
-let get_known_mp () =
-  match !known_mp with None -> failwith "uninit mps" | Some mps -> mps
 
 open Json
 open Yojson.Basic.Util
@@ -115,40 +66,37 @@ let load_meta meta_fname =
   let mode =
     match metaj |> member "mode" |> to_string with
     | "debug" ->
-        let get_bool field =
-          metaj |> member "debug_info" |> member field |> to_bool
-        in
-        Debug
-          {
-            show_preprocess = get_bool "show_preprocess";
-            show_typing = get_bool "show_typing";
-            show_queries = get_bool "show_queries";
-            (* we don't need this field *)
-            show_solving = false;
-            show_stat = get_bool "show_stat";
-            show_info = get_bool "show_others";
-            show_debug = (try get_bool "show_debug" with _ -> false);
-          }
+        DebugTags (metaj |> member "debug_tags" |> to_list |> List.map to_string)
     | "release" -> Release
     | _ -> failwith "config: unknown mode"
   in
   let max_printing_size = metaj |> member "max_printing_size" |> to_int in
   let resfile = metaj |> member "resfile" |> to_string in
   let logfile = metaj |> member "logfile" |> to_string in
+  let abdfile = metaj |> member "abdfile" |> to_string in
+  let abd_templates =
+    metaj |> member "abd_templates" |> to_list |> List.map to_string
+  in
+  let num_quantifier = metaj |> member "num_quantifier" |> to_int in
   let p = metaj |> member "prim_path" in
   let prim_path =
     {
       templates = p |> member "templates" |> to_string;
-      normalp = p |> member "normal_typing" |> to_string;
-      under_basicp = p |> member "builtin_coverage_typing" |> to_string;
-      under_randomp =
-        p |> member "builtin_randomness_coverage_typing" |> to_string;
-      underp_dir = p |> member "builtin_datatype_coverage_typing" |> to_string;
-      rev_underp_dir =
-        p |> member "rev_builtin_datatype_coverage_typing" |> to_string;
+      normal_typing = p |> member "normal_typing" |> to_string;
+      coverage_typing = p |> member "coverage_typing" |> to_string;
       type_decls = p |> member "data_type_decls" |> to_string;
-      lemmas = p |> member "axioms_of_predicates" |> to_string;
-      functional_lemmas = p |> member "axioms_of_query_encoding" |> to_string;
+      axioms = p |> member "axioms" |> to_string;
     }
   in
-  meta_config := Some { mode; max_printing_size; prim_path; logfile; resfile }
+  meta_config :=
+    Some
+      {
+        mode;
+        max_printing_size;
+        prim_path;
+        logfile;
+        resfile;
+        abdfile;
+        abd_templates;
+        num_quantifier;
+      }
