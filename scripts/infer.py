1c2ccac51159747865153863973017302e9856a1
zhouzhezz <zorichzorich@gmail.com>
2024-03-25 18:16:23 -0400

    add infer scripts

---
+++ scripts/infer.py
@@ -0,0 +1,28 @@
+import sys
+import argparse
+import os
+import subprocess
+import shutil
+import locale
+import run_bench
+import re
+
+cmd_prefix = ["dune", "exec", "--", "bin/main.exe"]
+config_file = "meta-config.json"
+workdir = ""
+verbose = True
+
+def run_type_infer_aux(meta_config_file, f):
+    cmd = cmd_prefix + ["type-infer", meta_config_file, f]
+    run_bench.invoc_cmd(verbose, cmd, None)
+
+def run_type_infer(dir_str):
+    meta_config_file = "{}/{}".format(dir_str, config_file)
+    for filename in os.listdir(dir_str):
+        matches = re.search(r"prog[0-9]+\.ml", filename)
+        if matches:
+            run_type_infer_aux(meta_config_file, "{}/{}".format(dir_str, filename))
+
+if __name__ == '__main__':
+    dir_str = sys.argv[1]
+    run_type_infer(dir_str)
--- scripts/validate.py
+++ scripts/validate.py
@@ -11,19 +11,19 @@ meta_config_file = "meta-config.json"
 workdir = ""
 verbose = True
 
-def run_type_check(f):
+def run_type_check_aux(f):
     cmd = cmd_prefix + ["type-check", meta_config_file, f]
     run_bench.invoc_cmd(verbose, cmd, None)
 
-def run(dir_str):
+def run_type_check(dir_str):
     if os.path.isdir(dir_str):
         for f in os.listdir(dir_str):
             run("{}/{}".format(dir_str, f))
     else:
         _, postfix = os.path.splitext(dir_str)
         if postfix == ".ml":
-            run_type_check(dir_str)
+            run_type_check_aux(dir_str)
 
 if __name__ == '__main__':
     dir_str = sys.argv[1]
-    run(dir_str)
+    run_type_check(dir_str)
